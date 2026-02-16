bc6542077b271257e2e27e2fa26769f75ad563e4
amamam1231 <securevpntg@gmail.com>
2026-02-16 16:40:20 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['Georgia', 'Cambria', 'Times New Roman', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
